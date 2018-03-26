e5fc0d5e9525431a83405a8ed4b3aa384b63d411
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2018-03-26 12:45:16 -0300

    Add files via upload

---
+++ FaceCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+
+		cam = Camera.main;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward, cam.transform.rotation * Vector3.up);
+
+	}
+}
--- PlayerSetup.cs
+++ PlayerSetup.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class PlayerSetup : NetworkBehaviour
 {
+
+	public Color playerColor;
+	public string baseName = "PLAYER";
+	public int playerNum = 1;
+	public Text playerNameText;
 
 	// Use this for initialization
 	void Start () {
@@ -14,5 +20,24 @@ public class PlayerSetup : NetworkBehaviour
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public override void OnStartClient()
+	{
+		base.OnStartClient();
+		playerNameText.enabled = false;
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		base.OnStartLocalPlayer();
+
+		MeshRenderer[] meshes = GetComponentsInChildren<MeshRenderer>();
+		foreach (MeshRenderer m in meshes)
+		{
+			m.material.color = playerColor;
+		}
+		playerNameText.enabled = true;
+		playerNameText.text = baseName + " " + playerNum;
 	}
 }
